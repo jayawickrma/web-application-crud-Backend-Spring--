9a4491a4d6bee18c47bdde3ec82f3e77da2e9696
jayawickrma <142202862+jayawickrma@users.noreply.github.com>
2024-08-07 00:44:04 +0530

    created pakages and classes

--- src/main/java/com/example/backendcrud/controller/StudentController.java
+++ src/main/java/com/example/backendcrud/controller/StudentController.java
@@ -1,4 +1,26 @@
 package com.example.backendcrud.controller;
 
-public class StudentController {
+import com.example.backendcrud.entity.Student;
+import com.example.backendcrud.repository.studentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/student")
+@CrossOrigin
+public class StudentController{
+
+    @Autowired
+    private studentRepository studentRepository;
+
+    @GetMapping("/getAllPost")
+    public List<Student>getAllPost(){
+        System.out.println();
+        return null;
+    }
+
+    //@PostMapping("/addPost")
+   // public Student saveStudent(@RequestBody Student student)
 }
